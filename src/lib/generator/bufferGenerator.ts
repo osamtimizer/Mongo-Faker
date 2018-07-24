810d17a309dc211dce4daf182464568a8ab65603
osamtimizer <eaten.jb@gmail.com>
2018-07-24 10:42:53 +0900

    small fix

--- src/lib/generator/bufferGenerator.ts
+++ src/lib/generator/bufferGenerator.ts
@@ -1,6 +1,12 @@
 import IGenerator from "../interfaces/IGenerator";
+import { Schema } from 'mongoose';
 
 class BufferGenerator implements IGenerator<Buffer>{
+  private _schema: Schema.Types.Buffer;
+
+  constructor(schema: Schema.Types.Buffer) {
+    this._schema = schema;
+  }
   generate(): Buffer {
     return new Buffer('', 'utf-8');
   }
--- src/lib/generator/mapGenerator.ts
+++ src/lib/generator/mapGenerator.ts
@@ -1,7 +1,10 @@
 import IGenerator from "../interfaces/IGenerator";
+import { Schema } from 'mongoose';
 
 class MapGenerator<K, V> implements IGenerator<Map<K, V>>{
   generate(): Map<K, V> {
     return new Map<K, V>();
   }
 }
+
+export default MapGenerator;
